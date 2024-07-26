73c3c1a580669236bbb4a9fe19a50c40f162417e
Abban Khan <abbanisepic@gmail.com>
2024-07-26 18:39:34 +0200

    I think I need to have another API for emojis because there are some emojis missing with the one I am using

--- pages/js/emoji.js
+++ pages/js/emoji.js
@@ -1,11 +1,29 @@
-getEmoji();
+getPopularEmoji();
+
+const emojiSearch = document.querySelector("#emojiSearch");
+
+emojiSearch.addEventListener("input", function () {
+  const keywords = emojiSearch.value.trim();
+  //Only display the list after 500ms -> user stop typing
+  setTimeout(function () {
+    if (keywords.length == 0) {
+      getPopularEmoji();
+    } else if (keywords == emojiSearch.value) {
+      getSearchedEmoji(keywords);
+    }
+  }, 500);
+});
 
 function displayEmojies(data) {
   let emojis = document.querySelectorAll(".emoji");
   Array.from(emojis).forEach((emoji) => {
-    emoji.removeEventListener("click", function () {
-      messageTextArea.innerHTML += emoji.innerHTML;
-    });
+    emoji.removeEventListener(
+      "click",
+      function () {
+        messageTextArea.innerHTML += emoji.innerHTML;
+      },
+      true
+    );
   });
   // Get the template source
   const templateSource = document.getElementById(
@@ -33,8 +51,30 @@ function displayEmojies(data) {
   });
 }
 
-async function getEmoji() {
+async function getPopularEmoji() {
   fetch("https://api.emojisworld.fr/v1/popular")
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+      return response.json();
+    })
+    .then((emojis) => {
+      const emojiMap = new Map();
+      Object.entries(emojis["results"]).map((emoji) => {
+        const key = emoji[1]["name"];
+        const value = emoji[1]["emoji"];
+        emojiMap.set(key, value);
+      });
+      displayEmojies(emojiMap);
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+}
+
+async function getSearchedEmoji(query) {
+  fetch(`https://api.emojisworld.fr/v1/search?q=${query}`)
     .then((response) => {
       if (!response.ok) {
         throw new Error(`HTTP error! Status: ${response.status}`);
